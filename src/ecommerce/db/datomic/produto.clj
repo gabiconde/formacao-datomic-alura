fc2233bde6b92bac6e82622f94d18171176ead46
Gabriela Conde <gabriela.conde@nubank.com.br>
2020-08-10 20:19:29 -0300

    search with bound values

--- src/ecommerce/db/datomic/produto.clj
+++ src/ecommerce/db/datomic/produto.clj
@@ -4,6 +4,7 @@
 (defn find-all [db]
   (d/q '[:find ?entidade
          :where [?entidade :produto/nome]] db))
+;any entity that has the attribute :produto/nome
 
 (defn find-by-slug
   [db slug]
@@ -17,6 +18,14 @@
   (d/q '[:find ?slug
          :where [_ :produto/slug ?slug]] db))
 ;use _ when we don't care about the data
+
+(defn find-price-and-name
+  [db]
+  (d/q '[:find ?nome ?preco
+         :where [?produto :produto/preco ?preco]
+                [?produto :produto/nome ?nome]] db))
+;if the ?produto is no explicit the return will be a cartesian product
+; of all values it found [nome1, preco1] [nome1, preco2] [nome2, preco1] [nome2, preco2]
 
 (defn update-produto
   [conn id-entity attr value]
