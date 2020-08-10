2a7ab8ba04d81a68da49debeaa8ddd617fe674b7
Gabriela Conde <gabriela.conde@nubank.com.br>
2020-08-10 19:56:41 -0300

    queries with params

--- src/ecommerce/core.clj
+++ src/ecommerce/core.clj
@@ -2,30 +2,26 @@
   (:use clojure.pprint)
   (:require [datomic.api :as d]
             [ecommerce.db.datomic.config :as db]
-            [ecommerce.produto.model :as model]))
+            [ecommerce.produto.model :as model]
+            [ecommerce.db.datomic.produto :as produto]))
 
 (def conn (db/abre-conexao))
 
-(let [camera (model/novo-produto "Camera" "/camera" 2500.10M)]
-  (d/transact conn [camera]))
-;#datom [id-da-entidade atributo valor id-da-tx added?]
-;when datom is true in the last attribute it means that something was added to the db
-
-;two transactors (read/write)
-;conn is only for read
-(def db (d/db conn)) ;snapshot of db, only read
-(d/q '[:find ?entidade
-       :where [?entidade :produto/preco]] db)
-
-;our schema accept a insert with only one attribute
-(d/transact conn [{:produto/nome "Calculadora"}])
-
-;It is not possible to insert a nil value.
-;updating a value
-(let [celular (model/novo-produto "Celular" "/celular" 34.9M)
+(let [camera (model/novo-produto "Camera" "/camera" 2500.10M)
+      celular (model/novo-produto "Celular" "/celular" 34.9M)
       resultado @(d/transact conn [celular])
       id-entity (-> resultado :tempids vals first)]
+
   (pprint resultado)
-  (pprint @(d/transact conn [[:db/add id-entity :produto/preco 23.0M]]))
-  ;two datoms one is for the value removed and the other for the new value added
-  (pprint @(d/transact conn [[:db/retract id-entity :produto/slug "/celular"]])))
+  (pprint (produto/update-produto conn id-entity :produto/preco 23.0M))
+  ;(pprint (produto/remove conn id-entity :produto/slug "/celular"))
+
+  (d/transact conn [camera]))
+
+(def db (d/db conn))
+(pprint (produto/find-all db))
+(pprint (produto/find-by-slug db "/camera"))
+
+(d/transact conn [{:produto/nome "Calculadora"}])
+
+(db/apaga-banco)
--- src/ecommerce/db/datomic/produto.clj
+++ src/ecommerce/db/datomic/produto.clj
@@ -1,5 +1,21 @@
-(ns ecommerce.db.datomic.produto)
+(ns ecommerce.db.datomic.produto
+  (:require [datomic.api :as d]))
 
 (defn find-all [db]
   (d/q '[:find ?entidade
          :where [?entidade :produto/nome]] db))
+
+(defn find-by-slug
+  [db slug]
+  (d/q '[:find ?entidade
+         :in $ ?slug-buscado
+         :where [?entidade :produto/slug ?slug-buscado]]
+       db slug))
+
+(defn update-produto
+  [conn id-entity attr value]
+  @(d/transact conn [[:db/add id-entity attr value]]))
+
+(defn remove-produto
+  [conn id-entity attr value]
+  @(d/transact conn [[:db/retract id-entity attr value]]))
